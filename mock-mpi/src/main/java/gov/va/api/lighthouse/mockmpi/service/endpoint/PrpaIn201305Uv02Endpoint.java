523eac92fe340a9c492c9fc6b5cf6dc208217774
Emily Subocz <emily.subocz@libertyits.com>
2021-05-20 16:30:54 -0400

    Add edipi and icn 1305 search support

--- mock-mpi/src/main/java/gov/va/api/lighthouse/mockmpi/service/endpoint/PrpaIn201305Uv02Endpoint.java
+++ mock-mpi/src/main/java/gov/va/api/lighthouse/mockmpi/service/endpoint/PrpaIn201305Uv02Endpoint.java
@@ -33,13 +33,93 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 public class PrpaIn201305Uv02Endpoint {
   private static final String NAMESPACE_URI = "http://vaww.oed.oit.va.gov";
 
+  private static final String EDIPI_ROOT = "2.16.840.1.113883.3.42.10001.100001.12";
+
+  private static final String ICN_ROOT = "2.16.840.1.113883.4.349";
+
   @PersistenceContext private EntityManager entityManager;
+
+  /** Return the identifier present (ssn, edipi, or icn) in the request. */
+  private String getIdentifier(JAXBElement<PRPAIN201305UV02> request) {
+    String ssn = getSsn(request);
+    if (ssn != null && !ssn.isBlank()) {
+      return ssn;
+    }
+    String icn = getRequestId(request, ICN_ROOT);
+    if (icn != null && !icn.isBlank()) {
+      return icn;
+    }
+    String edipi = getRequestId(request, EDIPI_ROOT);
+    if (edipi != null && !edipi.isBlank()) {
+      return edipi;
+    }
+    return null;
+  }
+
+  /** Return ID present in request that matches the passed in root value. */
+  private String getRequestId(JAXBElement<PRPAIN201305UV02> request, String rootValue) {
+    String root =
+        Optional.ofNullable(request)
+            .map(req -> req.getValue())
+            .map(value -> value.getControlActProcess())
+            .map(controlActProcess -> controlActProcess.getQueryByParameter())
+            .map(queryByParameter -> queryByParameter.getValue())
+            .map(value -> value.getParameterList())
+            .map(parameterList -> parameterList.getId())
+            .map(id -> id.getRoot())
+            .orElse(null);
+    if (root != null && root.equals(rootValue)) {
+      return Optional.ofNullable(request)
+          .map(req -> req.getValue())
+          .map(value -> value.getControlActProcess())
+          .map(controlActProcess -> controlActProcess.getQueryByParameter())
+          .map(queryByParameter -> queryByParameter.getValue())
+          .map(value -> value.getParameterList())
+          .map(parameterList -> parameterList.getId())
+          .map(id -> id.getExtension())
+          .orElse(null);
+    }
+    return null;
+  }
+
+  @SneakyThrows
+  private JAXBElement<PRPAIN201306UV02> getResponse(String identifier) {
+    PrpaIn201306Uv02Entity responseEntity =
+        entityManager.find(PrpaIn201306Uv02Entity.class, identifier);
+    if (responseEntity == null) {
+      return null;
+    }
+    return JAXBContext.newInstance(PRPAIN201306UV02.class)
+        .createUnmarshaller()
+        .unmarshal(
+            new StreamSource(new StringReader(responseEntity.profile())), PRPAIN201306UV02.class);
+  }
+
+  private String getSsn(JAXBElement<PRPAIN201305UV02> request) {
+    String ssn =
+        Optional.ofNullable(request)
+            .map(req -> req.getValue())
+            .map(value -> value.getControlActProcess())
+            .map(controlActProcess -> controlActProcess.getQueryByParameter())
+            .map(queryByParameter -> queryByParameter.getValue())
+            .map(value -> value.getParameterList())
+            .map(parameterList -> parameterList.getLivingSubjectId())
+            .filter(livingSubjectIdList -> !livingSubjectIdList.isEmpty())
+            .map(livingSubjectIdList -> livingSubjectIdList.get(0))
+            .map(livingSubjectId -> livingSubjectId.getValue())
+            .filter(valueList -> !valueList.isEmpty())
+            .map(valueList -> valueList.get(0))
+            .map(ii -> ii.getExtension())
+            .orElse(null);
+    return ssn;
+  }
 
   /** Persist PRPAIN201306UV02 for each data file. */
   @Transactional
   @EventListener(ApplicationStartedEvent.class)
   public void initData() {
     persistResources("classpath*:data/PRPA_IN201306UV02/profile/*.xml");
+    persistResources("classpath*:data/PRPA_IN201306UV02/profile_edipi/*.xml");
     persistResources("classpath*:data/PRPA_IN201306UV02/profile_icn/*.xml");
   }
 
@@ -63,48 +143,16 @@ public class PrpaIn201305Uv02Endpoint {
   /** Get MPI PRPAIN201306UV02 Response. */
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "PRPA_IN201305UV02")
   @ResponsePayload
-  @SneakyThrows
   public JAXBElement<PRPAIN201306UV02> prpa_In201305Uv02Response(
       @RequestPayload JAXBElement<PRPAIN201305UV02> request) {
-    String ssn =
-        Optional.ofNullable(request)
-            .map(req -> req.getValue())
-            .map(value -> value.getControlActProcess())
-            .map(controlActProcess -> controlActProcess.getQueryByParameter())
-            .map(queryByParameter -> queryByParameter.getValue())
-            .map(value -> value.getParameterList())
-            .map(parameterList -> parameterList.getLivingSubjectId())
-            .filter(livingSubjectIdList -> !livingSubjectIdList.isEmpty())
-            .map(livingSubjectIdList -> livingSubjectIdList.get(0))
-            .map(livingSubjectId -> livingSubjectId.getValue())
-            .filter(valueList -> !valueList.isEmpty())
-            .map(valueList -> valueList.get(0))
-            .map(ii -> ii.getExtension())
-            .orElse(null);
-    if (ssn == null) {
-      return JAXBContext.newInstance(PRPAIN201306UV02.class)
-          .createUnmarshaller()
-          .unmarshal(
-              new StreamSource(
-                  new StringReader(
-                      entityManager
-                          .find(PrpaIn201306Uv02Entity.class, "invalid_request")
-                          .profile())),
-              PRPAIN201306UV02.class);
+    String identifier = getIdentifier(request);
+    if (identifier == null) {
+      return getResponse("invalid_request");
     }
-    PrpaIn201306Uv02Entity responseEntity = entityManager.find(PrpaIn201306Uv02Entity.class, ssn);
-    if (responseEntity == null) {
-      return JAXBContext.newInstance(PRPAIN201306UV02.class)
-          .createUnmarshaller()
-          .unmarshal(
-              new StreamSource(
-                  new StringReader(
-                      entityManager.find(PrpaIn201306Uv02Entity.class, "not_found").profile())),
-              PRPAIN201306UV02.class);
+    JAXBElement<PRPAIN201306UV02> response = getResponse(identifier);
+    if (response != null) {
+      return response;
     }
-    String profile = responseEntity.profile();
-    return JAXBContext.newInstance(PRPAIN201306UV02.class)
-        .createUnmarshaller()
-        .unmarshal(new StreamSource(new StringReader(profile)), PRPAIN201306UV02.class);
+    return getResponse("not_found");
   }
 }
