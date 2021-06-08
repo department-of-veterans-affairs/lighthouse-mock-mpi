87ac4857f539210a7dbb883a91dacaf349a1d0a8
esubocz-va <emily.subocz@libertyits.com>
2021-06-08 12:54:26 -0400

    remove unused imports

--- mock-mpi/src/test/java/gov/va/api/lighthouse/mockmpi/service/endpoint/PrpaIn201305Uv02EndpointTest.java
+++ mock-mpi/src/test/java/gov/va/api/lighthouse/mockmpi/service/endpoint/PrpaIn201305Uv02EndpointTest.java
@@ -16,11 +16,9 @@ import org.hl7.v3.PRPAMT201306UV02ParameterList;
 import org.hl7.v3.PRPAMT201306UV02QueryByParameter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-// import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-// import org.springframework.test.context.junit4.SpringRunner;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(
